24bb7a1abd0c1b63275b57676284d0609e2229cf
cesarsmallgrasset <105759347+cesarsmallgrasset@users.noreply.github.com>
2022-10-07 10:58:54 -0400

    Maps

---
+++ Assets/Project/Scripts/BottleGame/BottleScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleScript : MonoBehaviour
+{
+    private BottleManager bottlemanager;
+    [SerializeField] GameObject item;
+    private Transform StartPos;
+
+    private void Awake()
+    {
+        bottlemanager = FindObjectOfType<BottleManager>();
+
+    }
+
+    private void Start()
+    {
+        StartPos.SetPositionAndRotation(item.transform.position, item.transform.rotation);
+    }
+    private void Update()
+    {
+        GameRestart();
+    }
+    void GameRestart()
+    {
+        if (bottlemanager.Restart)
+        {
+            item.gameObject.transform.SetPositionAndRotation(StartPos.position, StartPos.rotation);
+            Debug.Log("Bottle Restart");
+        }
+    }
+}
