66992a14785e2c2bc07a308f53a7f79f200496c7
cesarsmallgrasset <105759347+cesarsmallgrasset@users.noreply.github.com>
2022-10-12 00:34:51 -0400

    Merged files

--- Assets/Project/Scripts/WaterGunGame/WaterGunHit.cs
+++ Assets/Project/Scripts/WaterGunGame/WaterGunHit.cs
@@ -1,29 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterGunHit : MonoBehaviour
 {
-    private WaterGunManager manager;
-    [SerializeField] internal GameObject Balloon;
-    [SerializeField] internal ParticleSystem water;
-    [SerializeField] internal Vector3 IncrementationValues;
-    internal float nbOfParticles = 0;
-    internal float x,y,z;
-    internal Vector3 itemSize;
-    
-    private void Awake()
-    {
-        manager = GameObject.FindObjectOfType<WaterGunManager>();
-        itemSize = Balloon.transform.localScale;
-    }
+    [Header("References")]
 
+    [Tooltip("Insert the name (or at least a part that is unique) of the particle that will affect this object")]
+    [SerializeField] private ParticleSystem Water;
+    [Tooltip("Insert the item you want to be blown up")]
+    [SerializeField] private GameObject item;
+    [Tooltip("Insert the sound you want")]
+    [SerializeField] private AudioSource victorySound, balloonPop;
+    //[Tooltip("Insert the animation you want")]
+    //reference the animation here
+
+
+    [Header("Variables")]
+
+
+    [Tooltip("Indicates a number at which the item will pop (delete)")]
+    [SerializeField] private float MaxPressure = 1000f;
+    [SerializeField] private Vector3 Values;
+    internal float nbOfParticles = 0;
+    internal bool Won = false;
 
     private void OnParticleCollision(GameObject other)
     {
-        if (other.name == water.name)
+
+        //Set the values to those of the object then increases the size on every call by the amount chosen in inspector
+        if (other.name.Contains(Water.name))
         {
-            //values being stored
-            Balloon.transform.localScale += (IncrementationValues/1000);
+
+            item.transform.localScale += (Values/1000);
+
             nbOfParticles++;
+
+            Debug.Log("hit");
+            if (nbOfParticles == MaxPressure)
+            {
+                Debug.Log("Finished!");
+                Destroy(item);
+                if (!victorySound.isPlaying)
+                {
+                    //insert animation
+                    victorySound.Play();
+                    this.gameObject.SetActive(false);
+                }
+                Won = true;
+            }
 
         }
     }
